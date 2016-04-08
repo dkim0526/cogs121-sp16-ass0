b1d023e4ffbb19fa4b00f484cf447451d450b837
Daniel Kim <bjkim0526@gmail.com>
2016-04-07 17:31:13 -0700

    changed some more stuff to make the website like hiring page

--- models.js
+++ models.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 var emailSchema = new mongoose.Schema({
+	"name": String,
 	"email": String,
+	"profile": String,
 	"content": String,
 	"created": Date
 });
--- routes/message.js
+++ routes/message.js
@@ -4,7 +4,9 @@ exports.send = function(req, res) {
   console.log(req.body); // help you see what is inside of req.body
     // your solution here
   var email = new models.email({
+  	name: req.body.name,
     email: req.body.email,
+    profile: req.body.profile,
     content: req.body.content,
     created: Date.now()
   });
